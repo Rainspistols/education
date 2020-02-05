d197627053674205feaae7186ff284f2b29fae16
Rainspistols <ffly@mail.ru>
2020-02-05 16:27:14 +0100

    array methods

--- js/alert.js
+++ js/alert.js
@@ -1,14 +1,27 @@
 "use strict";
 
-function getMaxSubSum(arr) {
-  let curSum = 0;
-  let maxSum = 0;
-  for (let num of arr) {
-    curSum += num;
-    maxSum = Math.max(curSum, maxSum);
-    if (curSum < 0) curSum = 0;
-  }
-  return maxSum;
+function unique(arr) {
+  let result = [];
+
+  arr.forEach(str => {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  });
+
+  return result;
 }
 
-console.log(getMaxSubSum([-1, 2, 3, -9]));
+let strings = [
+  "кришна",
+  "кришна",
+  "харе",
+  "харе",
+  "харе",
+  "харе",
+  "кришна",
+  "кришна",
+  ":-O"
+];
+
+console.log(unique(strings)); // кришна, харе, :-O
